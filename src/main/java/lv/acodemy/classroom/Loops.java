931ae15abc7c3e0ef547239c4b64dd438acac36e
KristinaSergejeva <kristina.sergejeva2020@gmail.com>
2023-03-17 11:26:00 +0200

    classroom_13032023]: Classroom progress changes

--- src/main/java/lv/acodemy/classroom/Loops.java
+++ src/main/java/lv/acodemy/classroom/Loops.java
@@ -1,13 +1,14 @@
 package lv.acodemy.classroom;
 
+import javax.sound.midi.Soundbank;
 import java.util.Arrays;
 
 public class Loops {
     public static void main(String[] args) {
 
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-
-        /*
+       
+         /*
         for(int i = 0; i < 10; i++) {
         // define counter;
         // condition
@@ -15,7 +16,7 @@ public class Loops {
          */
 
         for (int i = 0; i < 5; i++) {
-            System.out.println("Hello world!");
+            System.out.println("HelloWorld!");
         }
 
         //numbers[0] = 1
@@ -23,57 +24,60 @@ public class Loops {
 
         for (int i = 0; i < numbers.length; i++) {
             System.out.print(numbers[i] + " ");
+          //  System.out.println(numbers[i]);
         }
-        //numbers
+
+        //numbers[0] = 1;
+        //numbers[1] = 2;
+        //numbers[2] = 3;
+        //...numbers[9] = 10;
+        // 10 < 10 ! false!!
+
         System.out.println();
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) ;
-            {
-                System.out.println(numbers[i]);
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+        System.out.println("=======");
+
+        for (int i = numbers.length - 1; i > 0; i--) {
+            System.out.println(numbers[i]);
+        }
+
+        // Create String array with 10 names
+        // Print all names using for % for each
+
+        String[] names = {"Janis", "Marcis", "Peteris", "liga", "Livanis"};
+        for (int i = 0; i < names.length; i++) {
+            if (!names[i].equals("Peteris")) {
+                System.out.println(names[i]);
             }
+        }
+        System.out.println(Arrays.toString(names));
 
-            System.out.println("=======");
+        System.out.println("=========//=========");
+        for (String name:
+             names) {
+            System.out.println(name);
+        }
 
-            for (int number : numbers) {
-                System.out.println(number);
-            }
-
-            // Create String array with 10 names
-            // Print all names using for & for each
-
-            String[] names = {"Janis", "Marcis", "Peteris", "Liga", "Livanis"};
-            for (int j = 0; j < names.length; j++) {
-                if (!names[j].equals("Peteris")) {
-                    System.out.println(names[j]);
-                }
-            }
-            System.out.println(Arrays.toString(names));
-
-            System.out.println("==========/=========");
-
-            for (String name : names) {
-                System.out.println(name);
-            }
-
-            // do, do while
-
+        //do, do while
         /*
         do {
         // code to be executed
-        }(while is true)
-        */
+        }(while)
+         */
 
-            int k = 0;
-            do {
-                System.out.println(k);
-                k++;
-            } while (k <= 10);
+        int i = 0;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i <= 10);
 
-            int x = 0;
-            while (x <= 10) {
-                System.out.println(x);
-                x++;
-            }
+        int x = 0;
+        while (x <= 10) {
+            System.out.println(x);
+            x++;
+        }
 
         int[] arrayOfNum = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         int[] emptyArray = new int[arrayOfNum.length];
@@ -81,17 +85,35 @@ public class Loops {
         // move numbers from arrayOfNum to emptyArray using loop
         // emptyArray[0] = arrayOfNum[0]
 
-            for (int j = 0; j < arrayOfNum.length; j++) {
-                emptyArray[j] = arrayOfNum[j];
-            }
-                System.out.println(Arrays.toString(emptyArray));
-            }
-
-
-
-
+        for (int j = 0; j < arrayOfNum.length; j++) {
+            emptyArray[j] = arrayOfNum[j];
+            System.out.println(Arrays.toString(emptyArray));
         }
+        int j = 0;
+        do {
+            emptyArray[j] = arrayOfNum[j];
+            j++;
+        }while (j < arrayOfNum.length);
+
+
+
+
+
 
 
     }
+
+}
+
+
+
+        
+
+       
+    
+
+
+
+
+
 
